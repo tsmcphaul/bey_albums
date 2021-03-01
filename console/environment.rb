1c4204baff355bb549808e7f0f35bdf44ec2b885
Tammarra McPhaul <shaemc22@gmail.com>
2021-02-28 19:46:03 -0500

    commit on 'main'

---
+++ console/environment.rb
@@ -0,0 +1,9 @@
+require 'HTTParty'
+require 'pry'
+require 'json'
+
+
+require_relative "../lib/bey_albums/version"
+require_relative "../lib/bey_albums/api"
+require_relative "../lib/bey_albums/album"
+require_relative "../lib/bey_albums/cli"
--- lib/bey_albums.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "bey_albums/version"
-
-module BeyAlbums
-  class Error < StandardError; end
-  # Your code goes here...
-end
